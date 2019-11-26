c41939a65a7d05bb085589c51862a5c670a96648
liujunting <1446177808@qq.com>
2019-11-26 14:45:59 +0800

    commit

---
+++ miaomiao/app/src/main/java/com/hbsd/rjxy/miaomiao/ljt/login/PhoneLoginActivity.java
@@ -0,0 +1,16 @@
+package com.hbsd.rjxy.miaomiao.ljt.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hbsd.rjxy.miaomiao.R;
+
+public class PhoneLoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_login);
+    }
+}
---
+++ miaomiao/app/src/main/java/com/hbsd/rjxy/miaomiao/ljt/login/ShowProblemActivity.java
@@ -0,0 +1,18 @@
+package com.hbsd.rjxy.miaomiao.ljt.login;
+
+//import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hbsd.rjxy.miaomiao.R;
+
+public class ShowProblemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_problem);
+    }
+}
