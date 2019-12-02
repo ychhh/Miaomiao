9e2a7f36b2607d98cef4020f571464adbc981778
liujunting <1446177808@qq.com>
2019-12-02 16:54:16 +0800

    登录完善后上传

--- miaomiao_java/src/main/java/com/hbsd/rjxy/miaomiao/ljt/Login/controller/LoginController.java
+++ miaomiao_java/src/main/java/com/hbsd/rjxy/miaomiao/ljt/Login/controller/LoginController.java
@@ -22,6 +22,11 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
+    /**
+     * 密码登录
+     * @param request
+     * @param response
+     */
     @RequestMapping("/password")
     @ResponseBody
     public void loginUser(HttpServletRequest request, HttpServletResponse response){
@@ -41,23 +46,19 @@ public class LoginController {
                 System.out.println(phone+"====="+password);
                 User user=loginService.findUserByTel(phone);
                 if (user==null){
-                    //user 为空，表示未注册， 进行注册
-                    Date date=new Date();
-                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-                    String temp=sdf.format(date);
-                    Date time=null;
-                    try {
-                        time = sdf.parse(temp);
-                    } catch (ParseException e1) {
-                        e1.printStackTrace();
-                    }
-                    loginService.saveUser(phone,time);
-                    User userSave=loginService.findUserByTel(phone);
-                    res.put("result","true");
-                    res.put("uid",userSave.getUid());
+                    //user 为空，表示未注册， 提示用户进行验证码登录
+                    res.put("result","false");
                 }else {
-                    res.put("result","true");
-                    res.put("uid",user.getUid());
+                    if (user.getPwd()!=null){//用户已经设置密码
+                        if (user.getPwd().equals(password)){//密码正确
+                            res.put("result","true");
+                            res.put("uid",user.getUid());
+                        }else {
+                            res.put("result","error");
+                        }
+                    }else{//用户未设置密码，提示用户手机验证码登录，可登录后设置密码
+                        res.put("result","null");
+                    }
                 }
             }
             is.close();
@@ -69,6 +70,12 @@ public class LoginController {
         }
     }
 
+
+    /**
+     * 手机验证码方式登录
+     * @param request
+     * @param response
+     */
     @RequestMapping("/phone")
     @ResponseBody
     public void loginUserForPhone(HttpServletRequest request, HttpServletResponse response){
@@ -86,10 +93,23 @@ public class LoginController {
                 String phone = object.getString("tel");
                 User user=loginService.findUserByTel(phone);
                 if (user==null){
-                    //user 为空，表示未注册， 提醒用户使用手机验证码方式注册
-                    res.put("result","false");
+                    //user 为空，表示未注册， 进行注册
+                    Date date=new Date();
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                    String temp=sdf.format(date);
+                    Date time=null;
+                    try {
+                        time = sdf.parse(temp);
+                    } catch (ParseException e1) {
+                        e1.printStackTrace();
+                    }
+                    loginService.saveUser(phone,time);
+                    User userSave=loginService.findUserByTel(phone);
+                    res.put("result","true");
+                    res.put("uid",userSave.getUid());
                 }else {
-
+                    res.put("result","true");
+                    res.put("uid",user.getUid());
                 }
             }
             is.close();
