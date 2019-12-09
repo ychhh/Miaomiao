90840157d0d55107299e927b5f4b2d860bcad83b
liujunting <1446177808@qq.com>
2019-12-09 15:05:39 +0800

    Constant temp

--- miaomiao/app/src/main/java/com/hbsd/rjxy/miaomiao/utils/Constant.java
+++ miaomiao/app/src/main/java/com/hbsd/rjxy/miaomiao/utils/Constant.java
@@ -19,7 +19,7 @@ public class Constant {
     public static String ADD_HOT_URl = "http://10.7.87.224:8080/video/addHot";//点击小鱼干发送的Url
 
 
-    public static String LOGIN_URL="http://10.7.90.209:8080/login/";//登录的URL
+    public static String LOGIN_URL="http://10.7.88.102:8080/login/";//登录的URL
 
     public static int RECOMMEND_PAGE_DEFAULT = 1;   //推荐视频的当前页
 
