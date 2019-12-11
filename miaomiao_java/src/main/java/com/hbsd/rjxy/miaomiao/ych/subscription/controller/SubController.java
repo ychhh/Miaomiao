cf5d8df9dd8807e4f1e6fdabb40d4271136e4011
zhulichen <390597658@qq.com>
2019-12-11 16:34:19 +0800

    Revert "Merge branch 'master' of https://github.com/ychhh/Miaomiao"
    This reverts commit 6373887c894716275d124afbb918c4a00d200de9, reversing
    changes made to 6d76ce369b4307e85b738a0bfe43643b1f16a1be.

--- miaomiao_java/src/main/java/com/hbsd/rjxy/miaomiao/ych/subscription/controller/SubController.java
+++ miaomiao_java/src/main/java/com/hbsd/rjxy/miaomiao/ych/subscription/controller/SubController.java
@@ -1,25 +1,16 @@
 package com.hbsd.rjxy.miaomiao.ych.subscription.controller;
 
 import com.google.gson.Gson;
-import com.hbsd.rjxy.miaomiao.entity.Cat;
-import com.hbsd.rjxy.miaomiao.entity.Subscription_record;
-import com.hbsd.rjxy.miaomiao.ych.cat.service.CatService;
 import com.hbsd.rjxy.miaomiao.ych.subscription.service.SubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-import java.util.List;
-
 @RequestMapping("/sub")
 @Controller
 public class SubController {
     @Autowired
     private SubService subService;
-    @Autowired
-    private CatService catService;
     Gson gson = new Gson();
     @RequestMapping("/all")
     @ResponseBody
@@ -43,28 +34,14 @@ public class SubController {
     }
     @RequestMapping("/follow")
     @ResponseBody
-    public String follow(int uid,int cid){
-        return  gson.toJson(subService.follow(uid,cid));
+    public int follow(int uid,int cid){
+        return subService.follow(uid,cid);
     }
     @RequestMapping("/unfollow")
     @ResponseBody
     public int unfollow(int uid,int cid){
         return subService.unfollow(uid,cid);
     }
-    @RequestMapping("/findsubcatbyuid")
-    @ResponseBody
-    public String getSubCatByUid(int uid){
-        List<Cat> cats=new ArrayList<>();
-        List<Subscription_record> records=subService.findAllByUid(uid);
-        for (Subscription_record record :records){
-            System.out.println(record.toString());
-            System.out.println(record.toString());
-            if (record.getSubscription_status()==1) {
-                int cid = record.getCid();
-                cats.add(catService.findAllByCid(cid));
-            }
-        }
-        System.out.println(cats);
-        return gson.toJson(cats);
-    }
+
+
 }
