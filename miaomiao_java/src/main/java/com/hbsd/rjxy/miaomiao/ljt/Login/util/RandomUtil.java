261fbed0f0ff3c5f1f6b9cb07362b313e159d490
liujunting <1446177808@qq.com>
2019-12-17 09:33:08 +0800

    1217 完善登录

---
+++ miaomiao_java/src/main/java/com/hbsd/rjxy/miaomiao/ljt/Login/util/RandomUtil.java
@@ -0,0 +1,27 @@
+package com.hbsd.rjxy.miaomiao.ljt.Login.util;
+
+import java.util.Random;
+
+public class RandomUtil {
+    //生成随机用户名，数字和字母组成,
+    public static String getStringRandom() {
+        int length=12;
+        String val = "";
+        Random random = new Random();
+
+        //参数length，表示生成几位随机数
+        for(int i = 0; i < length; i++) {
+
+            String charOrNum = random.nextInt(2) % 2 == 0 ? "char" : "num";
+            //输出字母还是数字
+            if( "char".equalsIgnoreCase(charOrNum) ) {
+                //输出是大写字母还是小写字母
+                int temp = random.nextInt(2) % 2 == 0 ? 65 : 97;
+                val += (char)(random.nextInt(26) + temp);
+            } else if( "num".equalsIgnoreCase(charOrNum) ) {
+                val += String.valueOf(random.nextInt(10));
+            }
+        }
+        return val;
+    }
+}
